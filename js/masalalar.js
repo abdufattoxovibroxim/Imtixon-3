fd595f36764eff554c57344c7a684495505cba80
abdufattoxovibroxim <106995414+abdufattoxovibroxim@users.noreply.github.com>
2022-06-08 23:44:27 +0500

    Update masalalar.js

--- js/masalalar.js
+++ js/masalalar.js
@@ -46,3 +46,31 @@
 // array.unshift("Rap")
 
 // console.log(array);
+
+
+
+
+
+
+//Masala-3:
+
+
+let salaries = {
+  "Akmal": 1200,
+  "Salim": 5200,
+  "Karima": 1800
+}
+const  sum = []
+
+const calcPay = function(pay){
+
+    let paySalaries = 0
+    for(let p of pay){
+        paySalaries += p
+    }
+    sum.push(paySalaries)
+
+    return sum
+}
+
+console.log(calcPay(Object.values(salaries)));
