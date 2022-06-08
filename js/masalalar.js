d6cda04c061300f74d0798e405b8653532015d5c
abdufattoxovibroxim <106995414+abdufattoxovibroxim@users.noreply.github.com>
2022-06-08 22:34:13 +0500

    Update masalalar.js

--- js/masalalar.js
+++ js/masalalar.js
@@ -1,45 +1,48 @@
-//Masala-4:
-const arr = [1,3,3,3,5,6,2,8,9,0,11,12,13,14,15,16,17,18,19,20]
+// //Masala-4:
+// const arr = [1,3,3,3,5,6,2,8,9,0,11,12,13,14,15,16,17,18,19,20]
 
-function calcSum(arr) {
-    const toqArr = []
-    const juftArr =[]
-    for(let i of arr){
-        if(arr[i] % 2 === 0){
-            juftArr.push(arr[i])
-            // console.log(arr[i]);
-        }else{
-            toqArr.push(arr[i])
-        }
+// function calcSum(arr) {
+//     const toqArr = []
+//     const juftArr =[]
+//     for(let i of arr){
+//         if(arr[i] % 2 === 0){
+//             juftArr.push(arr[i])
+//             // console.log(arr[i]);
+//         }else{
+//             toqArr.push(arr[i])
+//         }
 
-    }
-    return `Juft array: ${juftArr}  Toq array: ${toqArr}`
-}
-console.log(calcSum(arr));
-
-
+//     }
+//     return `Juft array: ${juftArr}  Toq array: ${toqArr}`
+// }
+// console.log(calcSum(arr));
 
 
-//Masala-2:
-
-let array = ["Jazz", "Blues"]
-
-console.log(array);
-
-array.push("Rock-n-roll")
-
-console.log(array);
-
-array[Math.floor(array.length/2)] = "Classics"
-
-console.log(array);
-
-array.shift()
-
-console.log(array);
 
 
-array.unshift("Reggae")
-array.unshift("Rap")
 
-console.log(array);
+
+
+// //Masala-2:
+
+// let array = ["Jazz", "Blues"]
+
+// console.log(array);
+
+// array.push("Rock-n-roll")
+
+// console.log(array);
+
+// array[Math.floor(array.length/2)] = "Classics"
+
+// console.log(array);
+
+// array.shift()
+
+// console.log(array);
+
+
+// array.unshift("Reggae")
+// array.unshift("Rap")
+
+// console.log(array);
