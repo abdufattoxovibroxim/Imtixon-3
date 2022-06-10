9675c6188bb2b15faea7fcee75ff274a754bb8d1
abdufattoxovibroxim <106995414+abdufattoxovibroxim@users.noreply.github.com>
2022-06-10 09:57:33 +0500

    Update masalalar.js

--- js/masalalar.js
+++ js/masalalar.js
@@ -1,98 +1,98 @@
-// //Masala-4:
-// const arr = [1,3,3,3,5,6,2,8,9,0,11,12,13,14,15,16,17,18,19,20]
+//Masala-4:
+const arr = [1,3,3,3,5,6,2,8,9,0,11,12,13,14,15,16,17,18,19,20]
 
-// function calcSum(arr) {
-//     const toqArr = []
-//     const juftArr =[]
-//     for(let i of arr){
-//         if(arr[i] % 2 === 0){
-//             juftArr.push(arr[i])
-//             // console.log(arr[i]);
-//         }else{
-//             toqArr.push(arr[i])
-//         }
+function calcSum(arr) {
+    const toqArr = []
+    const juftArr =[]
+    for(let i of arr){
+        if(arr[i] % 2 === 0){
+            juftArr.push(arr[i])
+            // console.log(arr[i]);
+        }else{
+            toqArr.push(arr[i])
+        }
 
-//     }
-//     return `Juft array: ${juftArr}  Toq array: ${toqArr}`
-// }
-// console.log(calcSum(arr));
-
-
-
-
-
-
-
-// //Masala-2:
-
-// let array = ["Jazz", "Blues"]
-
-// console.log(array);
-
-// array.push("Rock-n-roll")
-
-// console.log(array);
-
-// array[Math.floor(array.length/2)] = "Classics"
-
-// console.log(array);
-
-// array.shift()
-
-// console.log(array);
-
-
-// array.unshift("Reggae")
-// array.unshift("Rap")
-
-// console.log(array);
+    }
+    return `Juft array: ${juftArr}  Toq array: ${toqArr}`
+}
+console.log(calcSum(arr));
 
 
 
 
 
 
-// //Masala-3:
+
+//Masala-2:
+
+let array = ["Jazz", "Blues"]
+
+console.log(array);
+
+array.push("Rock-n-roll")
+
+console.log(array);
+
+array[Math.floor(array.length/2)] = "Classics"
+
+console.log(array);
+
+array.shift()
+
+console.log(array);
 
 
-// let salaries = {
-//   "Akmal": 1200,
-//   "Salim": 5200,
-//   "Karima": 1800
-// }
-// const  sum = []
+array.unshift("Reggae")
+array.unshift("Rap")
 
-// const calcPay = function(pay){
-
-//     let paySalaries = 0
-//     for(let p of pay){
-//         paySalaries += p
-//     }
-//     sum.push(paySalaries)
-
-//     return sum
-// }
-
-// console.log(calcPay(Object.values(salaries)));
+console.log(array);
 
 
 
 
-// //Masala-1:
 
-// let arr = [];
-// const a = parseInt(prompt("Raqam kiriting:")),
-// const b = parseInt(prompt("Raqam kiriting:")),
-// const c = parseInt(prompt("Raqam kiriting:"));
 
-// arr.push(a,b,c);
+//Masala-3:
 
-// function sumInput(arr) {
-//     let sum = 0;
-//     for (let i = 0; i < arr.length; i++) {
-//         sum += arr[i];
-//     }
-//     return sum === NaN ? "son emas" : sum;
-// }
 
-// console.log(sum(arr));
+let salaries = {
+  "Akmal": 1200,
+  "Salim": 5200,
+  "Karima": 1800
+}
+const  sum = []
+
+const calcPay = function(pay){
+
+    let paySalaries = 0
+    for(let p of pay){
+        paySalaries += p
+    }
+    sum.push(paySalaries)
+
+    return sum
+}
+
+console.log(calcPay(Object.values(salaries)));
+
+
+
+
+//Masala-1:
+
+let arr = [];
+const a = parseInt(prompt("Raqam kiriting:")),
+const b = parseInt(prompt("Raqam kiriting:")),
+const c = parseInt(prompt("Raqam kiriting:"));
+
+arr.push(a,b,c);
+
+function sumInput(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum === NaN ? "son emas" : sum;
+}
+
+console.log(sum(arr));
